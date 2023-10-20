d8869857333136d502aad947f103d0d85431fcd6
Shashikant <shashikant.tripathi187@gmail.com>
2023-10-21 00:32:36 +0530

    Initial Commit!

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">App Work</div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "HomeView",
+  // components: {// HelloWorld,
+  // },
+};
+</script>
